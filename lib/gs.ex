46dfa085ff8132ea207a274f0de34db399f5f3d2
Sokovikov <skv-headless@yandex.ru>
2017-11-21 11:20:03 +0400

    socket

---
+++ lib/gs.ex
@@ -0,0 +1,32 @@
+defmodule GameServer do
+  require Socket
+
+  def listen(port) do
+    listen(port, &handler/1)
+  end
+
+  def listen(port, handler) do
+    IO.puts "listen"
+    Socket.TCP.listen!(port, packet: :line)
+    |> accept(handler)
+  end
+
+  def accept(listening_socket, handler) do
+    IO.puts "accept"
+    socket = Socket.TCP.accept!(listening_socket)
+    spawn(fn -> handle(socket, handler) end)
+    accept(listening_socket, handler)
+  end
+
+  def handle(socket, handler) do
+    IO.puts "handle"
+    incoming = Socket.Stream.recv!(socket)
+    socket |> Socket.Stream.send!(handler.(incoming))
+    handle(socket, handler)
+  end
+
+  def handler(line) do
+    String.upcase(line)
+  end
+end
+
